b988c7ee2806bc3993b6a962b115e8f800b5a8d7
Milos <m@deva.co>
2019-06-23 15:34:00 +0200

    Output ip and ip as json object.

--- app/app.js
+++ app/app.js
@@ -3,7 +3,15 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Woo hoo!')
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+  if (typeof req.query.text !== 'undefined') {
+    res.setHeader('Content-Type', 'text/plain')
+    return res.end(ip)
+  }
+
+  res.setHeader('Content-Type', 'application/json')
+  return res.end(JSON.stringify({ ip }))
 })
 
 export default app
--- test/app/app.js
+++ test/app/app.js
@@ -6,4 +6,22 @@ describe('Test the root path', () => {
     const response = await request(app).get('/')
     expect(response.statusCode).toBe(200)
   })
+
+  test('It should response localhost IP in JSON', async () => {
+    return await request(app)
+      .get('/')
+      .expect('Content-Type', /json/)
+      .then(response => {
+        expect(response.body.ip).toBe('::ffff:127.0.0.1')
+      })
+  })
+
+  test('It should response localhost IP in plain text', async () => {
+    return await request(app)
+      .get('/?text')
+      .expect('Content-Type', /text/)
+      .then(response => {
+        expect(response.text).toBe('::ffff:127.0.0.1')
+      })
+  })
 })
